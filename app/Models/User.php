1e024163e111f6e3d55112d901fe6ff5a68e6773
Gustavo <administrador@sqlapio.com>
2024-11-24 16:26:21 -0400

    corrigiendo correo electronico para usuarios administradores

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,7 +58,7 @@ class User extends Authenticatable implements FilamentUser
     public function canAccessPanel(Panel $panel): bool
     {
         if ($panel->getId() === 'admin') {
-            return str_ends_with($this->email, '@test.com') && $this->hasVerifiedEmail();
+            return str_ends_with($this->email, '@ciudadalternativa.com') && $this->hasVerifiedEmail();
         }
  
         return true;
