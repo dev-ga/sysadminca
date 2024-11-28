611f187b01f587287c2c1c98d766ff17d8f7d917
Gustavo <administrador@sqlapio.com>
2024-11-28 19:34:21 -0400

    cambios en la interfaz de usuario

--- resources/views/livewire/tables/table-inventory.blade.php
+++ resources/views/livewire/tables/table-inventory.blade.php
@@ -1,3 +1,3 @@
-<div class="dark boder p-4">
+<div class="boder p-4">
     {{ $this->table }}
 </div>
--- resources/views/livewire/tables/table-list-sale.blade.php
+++ resources/views/livewire/tables/table-list-sale.blade.php
@@ -1,6 +1,3 @@
-<div class="dark boder p-4">
-    <div class="flex">
-        <h1 class="text-lg mb-4 font-bold text-white uppercase">ventas on-line</h1>
-    </div>
+<div class="boder p-4">
     {{ $this->table }}
 </div>
